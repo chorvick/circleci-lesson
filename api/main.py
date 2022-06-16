9be875cd37f3f5449739e22b2589637632674efd
Justin Rice <jsrice7391@gmail.com>
2022-06-15 23:14:52 -0400

    Added the docs

--- api/main.py
+++ api/main.py
@@ -9,7 +9,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/users/{user_id}")
 def read_user(user_id: int, q: Union[str, None] = None):
     return {"user_id": user_id, "q": q}
