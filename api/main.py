351775989cd0c1dc6c0c1aa2a97d4d4a3ecb3278
you know who <chorvickprocode@gmail.com>
2022-06-23 13:46:04 -0400

    try to debug python to make work local

--- api/main.py
+++ api/main.py
@@ -3,13 +3,34 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
+'''fast api not working locally'''
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/users/{user_id}")
 def read_user(user_id: int, q: Union[str, None] = None):
     return {"user_id": user_id, "q": q}
-    
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, q: Union[str, None] = None):
+    return {"user_id": user_id, "q": q}
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, q: Union[str, None] = None):
+    return {"user_id": user_id, "q": q}
